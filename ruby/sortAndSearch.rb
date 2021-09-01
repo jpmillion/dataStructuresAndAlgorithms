ac77af12722f0c5f7cf9b26815245c95dd40e629
john million <jpmillion@comcast.net>
2021-09-01 12:17:58 -0400

    Define ruby version of mergeSort algorithm that uses O(n) space

--- ruby/sortAndSearch.rb
+++ ruby/sortAndSearch.rb
@@ -1,12 +1,12 @@
-def mergeSort(arr)
+def mergeSortA(arr)
     if arr.length < 2
         return arr 
     end
 
     midpoint = ((arr.length - 1) / 2).floor
 
-    left = mergeSort(arr[..midpoint])
-    right = mergeSort(arr[midpoint + 1...arr.length])
+    left = mergeSortA(arr[..midpoint])
+    right = mergeSortA(arr[midpoint + 1...arr.length])
 
     
     merge(left, right)
@@ -30,4 +30,37 @@ def merge(a1, a2)
     mergedArr
 end
 
-puts mergeSort([3, 7, -7, 11, 4, -7, 1, 0, -1, 13])
+def mergeSortB(arr)
+    return arr if arr.length < 2
+
+    mergeSortHelper(arr, 0, arr.length - 1, arr[...arr.length])
+    arr
+end
+
+def mergeSortHelper(arr, start, finish, arrCopy)
+    return if start >= finish
+    midpoint = midpoint = ((start + finish) / 2).floor
+
+    mergeSortHelper(arrCopy, start, midpoint, arr)
+    mergeSortHelper(arrCopy, midpoint + 1, finish, arr)
+
+    mergeB(arr, start, midpoint, finish, arrCopy)
+end
+
+def mergeB(arr, start, midpoint, finish, arrCopy)
+    left = start
+    right = midpoint + 1
+
+    for i in start..finish
+        if right > finish || (left <= midpoint && arrCopy[left] < arrCopy[right])
+            arr[i] = arrCopy[left]
+            left += 1
+        else
+            arr[i] = arrCopy[right]
+            right += 1
+        end
+    end
+end
+
+puts mergeSortA([3, 7, -7, 11, 4, -7, 1, 0, -1, 13])
+puts mergeSortB([3, 7, -7, 11, 4, -7, 1, 0, -1, 13])
