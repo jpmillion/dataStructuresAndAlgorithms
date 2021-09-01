8ff01162d2f8fe3e3863d6d66c1863b9dee62114
john million <jpmillion@comcast.net>
2021-09-01 19:18:36 -0400

    Define factorial algorithm in javascript

--- javascript/mathAlgos.js
+++ javascript/mathAlgos.js
@@ -13,6 +13,22 @@ function fib(n) {
     return nMinusOne + nMinusTwo;
 }
 
+function fac(n) {
+    if (n < 0) return 'invalid input';
+
+    let product = 1;
+
+    for (let i = n; i > 1; i--) {
+        product *= i;
+    }
+
+    return product;
+}
+
 for (let i = -1; i < 10; i++) {
     console.log(fib(i));
+}
+
+for (let i = -1; i < 11; i++) {
+    console.log(fac(i));
 }
