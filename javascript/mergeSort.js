ed59df0feb50f45bd000a71d9edbf5657b309ef1
john million <jpmillion@comcast.net>
2021-08-31 20:34:16 -0400

    Define different version of MergeSort that uses O(n) space

--- javascript/mergeSort.js
+++ javascript/mergeSort.js
@@ -19,5 +19,40 @@ function merge(a1, a2){
     return a3;
 }
 
+function mergeSort2(arr) {
+    if (arr.length < 2) return arr;
+
+    const arrCopy = [...arr];
+    mergeSortHelper(arr, 0, arr.length - 1, arrCopy);
+    return arr;
+}
+
+function mergeSortHelper(arr, start, end, arrCopy) {
+    if (start === end) return;
+    const midPoint = Math.floor((start + end) / 2);
+
+    mergeSortHelper(arrCopy, start, midPoint, arr);
+    mergeSortHelper(arrCopy, midPoint + 1, end, arr);
+
+    mergeInPlace(arr, start, midPoint, end, arrCopy);
+}
+
+function mergeInPlace(arr, start, midPoint, end, arrCopy) {
+    let leftIdx = start;
+    let rightIdx = midPoint + 1;
+
+    for (let i = start; i <= end; i++) {
+        if (rightIdx > end || (leftIdx <= midPoint && arrCopy[leftIdx] < arrCopy[rightIdx])) arr[i] = arrCopy[leftIdx++];
+        else arr[i] = arrCopy[rightIdx++];
+    }
+}
+
 arr = mergeSort1([-9, 9, -8, 8, 7, 6, -7, -6]);
 console.log(arr);
+
+arr = mergeSort2([-9, 9, -8, 8, 7, 6, -7, -6]);
+console.log(arr);
+
+
+
+
