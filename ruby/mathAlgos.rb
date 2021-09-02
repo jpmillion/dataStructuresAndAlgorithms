afb41076db66c4fb96219631fb66f66c34252134
john million <jpmillion@comcast.net>
2021-09-01 20:06:53 -0400

    Define isPrime? algorithm in ruby

--- ruby/mathAlgos.rb
+++ ruby/mathAlgos.rb
@@ -23,6 +23,17 @@ def fac(n)
     product
 end
 
+def isPrime?(n)
+    return 'invalid input' if n < 0
+    return false if n < 2 || (n % 2 == 0 && n != 2)
+
+    squareRoot = Math.sqrt(n).floor
+
+    (3..squareRoot).step(2) { |num| return false if n % num ==0 }
+
+    true
+end
+
 for i in -1..10 do
     puts i.to_s + ': ' + fib(i).to_s
 end
@@ -30,3 +41,5 @@ end
 for i in -1..10 do
     puts i.to_s + ': ' + fac(i).to_s
 end
+
+(-1...100).step { |num| puts num.to_s + ': ' + isPrime?(num).to_s }
