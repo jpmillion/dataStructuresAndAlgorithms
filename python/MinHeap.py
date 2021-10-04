987c3cfbe72b9f834fb562db92d4599de7556110
john million <jpmillion@comcast.net>
2021-10-04 19:49:55 -0400

    Define siftDown instance method

--- python/MinHeap.py
+++ python/MinHeap.py
@@ -3,3 +3,14 @@ class MinHeap:
     def __init__(self):
         self.heap = []
     
+    def siftDown(self, parentIdx):
+        heap = self.heap
+        firstChildIdx = parentIdx * 2 + 1
+        secondChildIdx = parentIdx * 2 + 2 if parentIdx * 2 + 2 < len(heap) else -1 
+
+        # if parent has one child or first child is less than second child, 
+        # the first child will be the parentIdx to compare; else the second child
+        idxToCompare = firstChildIdx if secondChildIdx == -1 or heap[firstChildIdx] < heap[secondChildIdx] else secondChildIdx
+
+        if heap[idxToCompare] < heap[parentIdx]:
+            heap[idxToCompare], heap[parentIdx] = heap[parentIdx], heap[idxToCompare]
