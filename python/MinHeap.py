29a7e737847b544e6eeb7ec1fe9208319c5d22e2
John Million <jpmillion@comcast.net>
2021-10-05 19:22:43 -0400

    Define remove instance method and refractor siftDown instance method

--- python/MinHeap.py
+++ python/MinHeap.py
@@ -14,11 +14,29 @@ class MinHeap:
     def siftDown(self, parentIdx):
         heap = self.heap
         firstChildIdx = parentIdx * 2 + 1
-        secondChildIdx = parentIdx * 2 + 2 if parentIdx * 2 + 2 < len(heap) else -1 
+        while firstChildIdx < len(heap):
+            secondChildIdx = parentIdx * 2 + 2 if parentIdx * 2 + 2 < len(heap) else -1 
 
-        # if parent has one child or first child is less than second child, 
-        # the first child will be the parentIdx to compare; else the second child
-        idxToCompare = firstChildIdx if secondChildIdx == -1 or heap[firstChildIdx] < heap[secondChildIdx] else secondChildIdx
+            # if parent has one child or first child is less than second child, 
+            # the first child will be the parentIdx to compare; else the second child
+            idxToCompare = firstChildIdx if secondChildIdx == -1 or heap[firstChildIdx] < heap[secondChildIdx] else secondChildIdx
 
-        if heap[idxToCompare] < heap[parentIdx]:
-            heap[idxToCompare], heap[parentIdx] = heap[parentIdx], heap[idxToCompare]
+            if heap[idxToCompare] < heap[parentIdx]:
+                heap[idxToCompare], heap[parentIdx] = heap[parentIdx], heap[idxToCompare]
+                parentIdx = idxToCompare
+                firstChildIdx = parentIdx * 2 + 1
+
+            else: 
+                return
+
+    def remove(self):
+        heap = self.heap
+        endIdx = len(heap) - 1
+
+        heap[0], heap[endIdx] = heap[endIdx], heap[0]
+
+        minElement = heap.pop()
+
+        self.siftDown(0)
+        
+        return minElement
