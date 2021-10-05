79e8f6c774c97df05d36076026acbc566867a928
John Million <jpmillion@comcast.net>
2021-10-05 19:23:56 -0400

    Test MinHeap remove method and refractor buildHeap test

--- python/testMinHeap.py
+++ python/testMinHeap.py
@@ -5,9 +5,17 @@ class TestMinHeap(unittest.TestCase):
 
     def testBuildHeap(self):
         arr = [9,5,8,1,3,6,7,11,-8,-9,15]
-        actual = [-9,9,6,-8,5,8,7,11,1,3,15]
+        actual = [-9,-8,6,1,3,8,7,11,9,5,15]
         minHeap = MinHeap(arr)
-        self.assertEquals(actual, minHeap.heap)
+        self.assertEqual(actual, minHeap.heap)
+
+    def testRemove(self):
+        arr = [9,5,8,1,3,6,7,11,-8,-9,15]
+        actual = [-8,1,6,9,3,8,7,11,15,5]
+        minHeap = MinHeap(arr)
+        minElement = minHeap.remove()
+        self.assertEqual(actual, minHeap.heap)
+        self.assertEqual(-9, minElement)
 
 
 if __name__ == '__main__':
